f75b2820af362fb135f390d976c577fea8577ba4
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-12 12:47:37 -0700

    update import types, add data-files

--- server.js
+++ server.js
@@ -1,42 +1,40 @@
-const movies = require("./movies-data.js");
-const trailers = require("./trailers-data.js");
 
-const express = require('express');
-var cors = require('cors')
+import movies from './movies-data.js';
+import trailers from './trailers-data.js';
+
+import express from 'express';
+import cors from 'cors';
 const app = express();
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Movies Data';
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (request, response) => {
-  response.send('Oh hey there');
+    response.send('Oh hey there');
 });
 
 app.get('/api/v1/movies', (request, response) => {
-  response.json({ movies }); 
+    response.json({ movies });
 });
 
 app.get('/api/v1/movies/:id', cors(), (request, response) => {
-  const movieId = request.params.id;
-  const movie = movies.find(movie => {
-    return parseInt(movieId) === movie.id;
-  });
-  response.json({ movie });
+    const movieId = request.params.id;
+    const movie = movies.find((movie) => {
+        return parseInt(movieId) === movie.id;
+    });
+    response.json({ movie });
 });
 
 app.get('/api/v1/movies/:id/videos', cors(), (request, response) => {
-  const movieId = request.params.id;
-  const videos = trailers.find(movieTrailer => {
-    return movieTrailer[movieId]
-  });
-  response.json(videos[movieId]);
+    const movieId = request.params.id;
+    const videos = trailers.find((movieTrailer) => {
+        return movieTrailer[movieId];
+    });
+    response.json(videos[movieId]);
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
-
-
-
